da122167b6f92b4f9ead729338f7bec9f36f2a17
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-07-20 21:22:18 -0300

    Create SiteApplication class for Spring Boot app

---
+++ src/main/java/com/exemplo/site/SiteApplication.java
@@ -0,0 +1,11 @@
+package com.exemplo.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SiteApplication.class, args);
+    }
+}
